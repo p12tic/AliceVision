abd783f2fadfc209362a1a5e0b37abdeb6748393
Povilas Kanapickas <povilas@radix.lt>
2022-09-30 04:28:49 +0300

    [camera] Reduce the number of includes that camera::EINTRINSIC brings

--- src/aliceVision/camera/cameraCommon.hpp
+++ src/aliceVision/camera/cameraCommon.hpp
@@ -8,12 +8,7 @@
 #pragma once
 
 #include <boost/detail/bitmask.hpp>
-#include <boost/algorithm/string.hpp>
 
-#include <string>
-#include <stdexcept>
-#include <algorithm>
-#include <vector>
 #include <string>
 
 namespace aliceVision {
@@ -41,62 +36,12 @@ enum EINTRINSIC
 
 BOOST_BITMASK(EINTRINSIC)
 
-inline std::string EINTRINSIC_enumToString(EINTRINSIC intrinsic)
-{
-  switch(intrinsic)
-  {
-      case EINTRINSIC::PINHOLE_CAMERA: return "pinhole";
-      case EINTRINSIC::PINHOLE_CAMERA_RADIAL1: return "radial1";
-      case EINTRINSIC::PINHOLE_CAMERA_RADIAL3: return "radial3";
-      case EINTRINSIC::PINHOLE_CAMERA_3DERADIAL4: return "3deradial4";
-      case EINTRINSIC::PINHOLE_CAMERA_BROWN: return "brown";
-      case EINTRINSIC::PINHOLE_CAMERA_FISHEYE: return "fisheye4";
-      case EINTRINSIC::PINHOLE_CAMERA_FISHEYE1: return "fisheye1";
-      case EINTRINSIC::PINHOLE_CAMERA_3DEANAMORPHIC4: return "3deanamorphic4";
-      case EINTRINSIC::PINHOLE_CAMERA_3DECLASSICLD: return "3declassicld";
-      case EINTRINSIC::EQUIDISTANT_CAMERA: return "equidistant";
-      case EINTRINSIC::EQUIDISTANT_CAMERA_RADIAL3: return "equidistant_r3";
-      case EINTRINSIC::UNKNOWN:
-      case EINTRINSIC::VALID_PINHOLE:
-      case EINTRINSIC::VALID_EQUIDISTANT:
-      case EINTRINSIC::VALID_CAMERA_MODEL:
-          break;
-  }
-  throw std::out_of_range("Invalid Intrinsic Enum");
-}
+std::string EINTRINSIC_enumToString(EINTRINSIC intrinsic);
+EINTRINSIC EINTRINSIC_stringToEnum(const std::string& intrinsic);
 
-inline EINTRINSIC EINTRINSIC_stringToEnum(const std::string& intrinsic)
-{
-  std::string type = intrinsic;
-  std::transform(type.begin(), type.end(), type.begin(), ::tolower); //tolower
+std::ostream& operator<<(std::ostream& os, EINTRINSIC e);
 
-  if(type == "pinhole") return EINTRINSIC::PINHOLE_CAMERA;
-  if(type == "radial1") return EINTRINSIC::PINHOLE_CAMERA_RADIAL1;
-  if(type == "radial3") return EINTRINSIC::PINHOLE_CAMERA_RADIAL3;
-  if(type == "3deradial4") return EINTRINSIC::PINHOLE_CAMERA_3DERADIAL4;
-  if(type == "brown") return EINTRINSIC::PINHOLE_CAMERA_BROWN;
-  if(type == "fisheye4") return EINTRINSIC::PINHOLE_CAMERA_FISHEYE;
-  if(type == "fisheye1") return EINTRINSIC::PINHOLE_CAMERA_FISHEYE1;
-  if(type == "3deanamorphic4") return EINTRINSIC::PINHOLE_CAMERA_3DEANAMORPHIC4;
-  if(type == "3declassicld") return EINTRINSIC::PINHOLE_CAMERA_3DECLASSICLD;
-  if(type == "equidistant") return EINTRINSIC::EQUIDISTANT_CAMERA;
-  if(type == "equidistant_r3") return EINTRINSIC::EQUIDISTANT_CAMERA_RADIAL3;
-
-  throw std::out_of_range(intrinsic);
-}
-
-inline std::ostream& operator<<(std::ostream& os, EINTRINSIC e)
-{
-    return os << EINTRINSIC_enumToString(e);
-}
-
-inline std::istream& operator>>(std::istream& in, EINTRINSIC& e)
-{
-    std::string token;
-    in >> token;
-    e = EINTRINSIC_stringToEnum(token);
-    return in;
-}
+std::istream& operator>>(std::istream& in, EINTRINSIC& e);
 
 // Return if the camera type is a valid enum
 inline bool isValid(EINTRINSIC eintrinsic)
@@ -114,21 +59,7 @@ inline bool isEquidistant(EINTRINSIC eintrinsic)
     return EINTRINSIC::VALID_EQUIDISTANT & eintrinsic;
 }
 
-inline EINTRINSIC EINTRINSIC_parseStringToBitmask(const std::string& str, const std::string& joinChar = ",")
-{   
-    std::vector<std::string> intrinsicsVec;
-    boost::split(intrinsicsVec, str, boost::is_any_of(joinChar));
-    if(!intrinsicsVec.size())
-    {
-        throw std::invalid_argument("'" + str + "'can't be parsed to EINTRINSIC Bitmask."); 
-    }
-    EINTRINSIC mask = EINTRINSIC_stringToEnum(intrinsicsVec[0]);
-    for(const std::string& intrinsic : intrinsicsVec)
-    {
-        mask |= EINTRINSIC_stringToEnum(intrinsic);
-    }
-    return mask;
-}
+EINTRINSIC EINTRINSIC_parseStringToBitmask(const std::string& str, const std::string& joinChar = ",");
 
 } // namespace camera
 } // namespace aliceVision
