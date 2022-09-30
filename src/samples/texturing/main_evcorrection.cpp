07bf32bd922f90b03000e467c263b3a86c5ddaaf
Povilas Kanapickas <povilas@radix.lt>
2022-09-30 04:27:29 +0300

    [samples] Use vfs

--- src/samples/texturing/main_evcorrection.cpp
+++ src/samples/texturing/main_evcorrection.cpp
@@ -9,9 +9,9 @@
 #include <aliceVision/system/cmdline.hpp> 
 #include <aliceVision/image/io.hpp> 
 #include <aliceVision/image/pixelTypes.hpp> 
+#include <aliceVision/vfs/filesystem.hpp>
  
 #include <boost/program_options.hpp> 
-#include <boost/filesystem.hpp> 
  
 #include <string> 
 #include <sstream> 
@@ -26,7 +26,6 @@
 using namespace aliceVision; 
  
 namespace po = boost::program_options; 
-namespace fs = boost::filesystem; 
  
 namespace oiio = OIIO; 
  
@@ -89,11 +88,11 @@ int main(int argc, char **argv)
     sfmData::SfMData sfm_data; 
  
     int c = 0; 
-    fs::path folderPath(inputFolderPath); 
-    for(auto& file : fs::directory_iterator(folderPath)) 
+    vfs::path folderPath(inputFolderPath);
+    for(auto& file : vfs::directory_iterator(folderPath))
     { 
-        fs::path filePath = file.path(); 
-        if(fs::is_regular_file(filePath)) 
+        vfs::path filePath = file.path();
+        if (vfs::is_regular_file(filePath))
         { 
             int w, h; 
             const IndexT id_view = c, id_pose = c, id_intrinsic = 0, rigId = 0, subPoseId = 0; 
@@ -123,11 +122,11 @@ int main(int argc, char **argv)
         for(int pix = 0; pix < view.getWidth() * view.getHeight(); ++pix) 
             img(pix) *= evComp; 
  
-        ALICEVISION_LOG_INFO(fs::path(view.getImagePath()).stem()); 
+        ALICEVISION_LOG_INFO(vfs::path(view.getImagePath()).stem());
         ALICEVISION_LOG_INFO("  EV: " << view.getEv()); 
         ALICEVISION_LOG_INFO("  EV Compensation: " << evComp);
 
-        std::string outputPath = outputFilePath + fs::path(view.getImagePath()).stem().string() + ".EXR"; 
+        std::string outputPath = outputFilePath + vfs::path(view.getImagePath()).stem().string() + ".EXR";
         oiio::ParamValueList metadata = image::getMetadataFromMap(view.getMetadata()); 
         image::writeImage(outputPath, img,
                           image::ImageWriteOptions().toColorSpace(image::EImageColorSpace::LINEAR),
@@ -136,8 +135,8 @@ int main(int argc, char **argv)
  
 /* 
     // calculate EV for all images in the input folder 
-    fs::path folderPath(inputFolderPath); 
-    for(auto& filePath : fs::directory_iterator(folderPath)) 
+    vfs::path folderPath(inputFolderPath);
+    for(auto& filePath : fs.directory_iterator(fs, folderPath))
     { 
         Image img; 
         int w, h; 
